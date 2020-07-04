38725372c5e2790773321a9195dc99b2bfbe8114
Kevin Lupera <kevinlupera1997@outlook.com>
2020-07-04 14:39:34 -0500

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:sticket_demo/static.dart';
+
+import 'information.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final String title = 'WhatsApp Stickers Demo';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+            bottom: TabBar(
+              tabs: [
+                Tab(text: "Information"),
+                Tab(text: "Sticker Packs"),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              WhatsAppInformation(),
+              StaticContent(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
